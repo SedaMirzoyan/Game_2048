b4fb88dd518b13371599461370eb241b19fdd6f2
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-04-15 02:38:53 +0400

    Add files via upload

--- 2048_game.py
+++ 2048_game.py
@@ -36,7 +36,7 @@ class Game_2048:
 
         
     def move(self):
-        #flag = self.is_full()
+        flag = self.is_full()
         while(True):
             try:
                 if(m.kbhit()):
@@ -53,7 +53,7 @@ class Game_2048:
                             print("right")
                         elif (input_dir == b'H'):           #up arrow"
                             print("up")
-                            self.move_up()
+                            self.move_up(flag)
                             #self.print_board()
                             self.find_coords()
                             self.set_num()
@@ -104,79 +104,29 @@ class Game_2048:
             #if(((coord[0] != self.first_x) and (coord[0] != self.second_x)) and ((coord[1] != self.first_y) and (coord[1] != self.second_y))): 
             if(self.m_board[coord[0]][coord[1]] == 0):
                 self.m_board[coord[0]][coord[1]] = self.all_nums[ind]
-                #print("index = ", ind)
 
         print("all_coords = ", self.all_coords)
     
 
-
-    '''
-    def move_up(self):
-        print("calling move up")
-        i_first = self.first_x
-        i_second = self.second_x
-        n = len(self.m_board)
-
-        prev_first_x = self.first_x
-        prev_second_x = self.second_x
-        dir_key = "up"
-        i = n - 1
-        j = n - 1
-        sum = 0
-
-        #shift first element to the 1st row
-        while(i_first >= 0):
-            self.first_x = i_first 
-            i_first += self.directions[dir_key][0] 
-
-        #shift second element to the 1st row
-        while(i_second >= 0):
-            self.second_x = i_second
-            i_second += self.directions[dir_key][0] 
-
-        
-        for i in range(n-1, -1, -1): 
-            for j in range(n-1, -1, -1):
-                if(self.m_board[i][j] == 0):
-                    self.m_board[i-1][j] = self.m_board[i][j]
-                    self.m_board[i][j] = 0
-                    if(prev_first_x != 0):
-                        self.m_board[prev_first_x][j] = 0
-                    elif(prev_second_x != 0):
-                        self.m_board[prev_second_x][j] = 0
-                else:
-                    if(self.m_board[i-1][j] == self.m_board[i][j]):
-                        print("hii")
-                        sum = self.m_board[i-1][j] + self.m_board[i][j]
-                        self.m_board[i-1][j] = sum
-                        self.m_board[i][j] = 0
-
-
-        #self.m_board[prev_first_x][self.first_y] = 0
-        #self.m_board[prev_second_x][self.second_y] = 0
-
-        self.m_board[self.first_x][self.first_y] = first_num
-        self.m_board[self.second_x][self.second_y] = second_num
-        '''
-        
-    
 
 
     def is_full(self):
         for i in range(len(self.m_board)):
             for j in  range(len(self.m_board)):
                 if(self.m_board[i][j] == 0):
+                    print("Continue")
+                    return True
+                else:
+                    print("Board is full, Game over")
                     return False
-        return True
 
 
     
-    def move_up(self):
+    def move_up(self, flag):
         print("calling move up")
         x_coord = 0
         dir_key = "up"
         sum = 0
-
         '''
         ##working
         for ind, elem in enumerate(self.all_coords):    
@@ -188,7 +138,6 @@ class Game_2048:
                 print("self.all_coords[ind][0]",  self.all_coords[ind][0] , "= x_coord = ", x_coord, "index = ", ind)
         ##
         '''
-        tmp_coord = 0
 
         for ind, elem in enumerate(self.all_coords):    
             x_coord = elem[0]
@@ -198,18 +147,12 @@ class Game_2048:
                 prev_x_coord = x_coord
                 print("prev_x_coord ", prev_x_coord)
                 x_coord += self.directions[dir_key][0] 
-                #self.all_coords[ind][0] = x_coord              #b4
-                #current_x_coord = self.all_coords[ind][0]      #b4
                 current_x_coord = x_coord      
-                tmp_coord = current_x_coord                 #after
                 print("current_x_coord ", current_x_coord)
-                #print("self.all_coords[ind][0]",  self.all_coords[ind][0] , "= x_coord = ", x_coord, "index = ", ind)
                 if((self.m_board[current_x_coord][y_coord] == 0) and (self.m_board[prev_x_coord][y_coord] != 0)):
                     self.m_board[current_x_coord][y_coord] = self.m_board[prev_x_coord][y_coord]
                     self.m_board[prev_x_coord][y_coord] = 0
-                    tmp_coord = current_x_coord
-                    print("tmp_coord = ", tmp_coord)
-                    self.all_coords[ind][0] = current_x_coord           #after
+                    self.all_coords[ind][0] = current_x_coord           
                     #continue
                 elif((self.m_board[current_x_coord][y_coord] != self.m_board[prev_x_coord][y_coord])
                      and (self.m_board[current_x_coord][y_coord] != 0)):  #and (self.m_board[prev_x_coord][y_coord] != self.m_board[current_x_coord-1][y_coord])):
@@ -225,20 +168,13 @@ class Game_2048:
                     print("nums are equal")
                     sum = self.m_board[current_x_coord][y_coord] + self.m_board[prev_x_coord][y_coord]
                     self.m_board[current_x_coord][y_coord] = sum
-                    #if((self.m_board[prev_x_coord+1][y_coord] != 0)):
-                    #    self.m_board[current_x_coord+1][y_coord] = self.m_board[prev_x_coord+1][y_coord]
-                    #else:
-                    #    self.m_board[current_x_coord+1][y_coord] = 0
                     self.m_board[prev_x_coord][y_coord] = 0
-                    self.all_coords[ind][0] = current_x_coord           #after
-                    self.all_coords.pop()
-                    
+                    self.all_coords[ind][0] = current_x_coord          
+                    self.all_coords.pop()                 
                     break
                 
                 
      
-
-
 
 
     def move_left(self):   
